58b6583a881cb5333ffa39763b5655291654a05b
Frankie Fox Wagoosh <frank.fox-wagoosh@stu.ocsb.ca>
2022-09-19 19:26:37 +0000

    added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+# Created By: Your Frankie fox wagoosh
+# Date: Sept 19, 2022
+# This program calculates the area & perimeter of 6cm  X 7cm rectangle
+
+
+def main():
+    print("For a rectangle with a length 6cm and width 7cm:")
+    print("The area is : {}cm^2".format(6 * 7))
+    print("The perimeter is: {}cm".format(2 * (6 + 7)))
+
+
+if __name__ == "__main__":
+    main()
--- basic_math.py
+++ basic_math.py
@@ -10,6 +10,10 @@ def main():
     print("5*1 = {}".format(5 * 1))
     print("4+2 = {}".format(4 + 2))
     print("8/2 = {}".format(8 / 2))
+    print("3+4*4 = {}".format(3 + 4 * 4))
+    print("2^5 = {}".format(2 ^ 5))  # bitwise operator
+    print("5**3 = {}".format(5**2))  # exponent
+    print("20**0.7{}".format(20**0.7))
 
 
 if __name__ == "__main__":
