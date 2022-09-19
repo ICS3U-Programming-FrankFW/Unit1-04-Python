13e61c5d16f08d5ed5dafdee06ae7c7823a3c194
Frankie Fox Wagoosh <frank.fox-wagoosh@stu.ocsb.ca>
2022-09-19 18:47:54 +0000

    added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python3
+# Created By: Your Frankie fox wagoosh
+# Date: Sept 19, 2022
+# This program does basic math
+
+
+def main():
+    print("6-2 = {}".format(6 - 2))
+    print("4+2 = {}".format(4 + 2))
+    print("5*1 = {}".format(5 * 1))
+    print("4+2 = {}".format(4 + 2))
+    print("8/2 = {}".format(8 / 2))
+
+
+if __name__ == "__main__":
+    main()
